355e9608a72aabbe2c2879c9964bba5aaeba8a14
Oguz Pastirmaci <oguzp@outlook.com>
2019-06-05 16:37:25 -0700

    Add deployment without a bastion

---
+++ scripts/redis.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+
+initDNS="${name}0.${name}.${name}.oraclevcn.com"
+nodeDNS=$(hostname -f)
+REDIS_PORT=6379
+private_ip=$(hostname -i)
+
+# Create the list of nodes to join the cluster based on the number of instances
+n=${count}
+join=""
+
+for i in $(seq 0 $(($n > 0? $n-1: 0))); do 
+  nodes=$(host $name$i.$name.$name.oraclevcn.com | awk '{print $4}')
+  join="$${join}$${join:+ }$nodes:$REDIS_PORT"
+done
+
+# Setup firewall rules
+firewall-offline-cmd  --zone=public --add-port=6379/tcp
+firewall-offline-cmd  --zone=public --add-port=16379/tcp
+systemctl restart firewalld
+
+# Install wget and gcc
+yum install -y gcc wget
+
+# Download and compile Redis
+wget http://download.redis.io/releases/redis-5.0.5.tar.gz
+tar xvzf redis-5.0.5.tar.gz
+cd redis-5.0.5
+make install
+
+# Prepare redis.conf for clustering
+cp ./redis.conf /etc/redis.conf
+sed -i "s/^bind 127.0.0.1/bind $private_ip/g" /etc/redis.conf
+sed -i "s/^# cluster-enabled yes/cluster-enabled yes/g" /etc/redis.conf
+sed -i "s/^# cluster-config-file /cluster-config-file /g" /etc/redis.conf
+sed -i "s/^# cluster-node-timeout 15000/cluster-node-timeout 15000/g" /etc/redis.conf
+sed -i "s/^appendonly no/appendonly yes/g" /etc/redis.conf
+sed -i "s/^daemonize no/daemonize yes/g" /etc/redis.conf
+
+/usr/local/bin/redis-server /etc/redis.conf
+
+sleep 60
+
+if [[ $initDNS == $nodeDNS ]]
+then
+    echo "yes" | redis-cli --cluster create $join --cluster-replicas 1
+fi
---
+++ terraform/redis.tf
@@ -0,0 +1,34 @@
+data "template_file" "user_data" {
+  template = "${file("../scripts/redis.sh")}"
+
+  vars {
+    count = "${var.instance["instance_count"]}"
+    name  = "${var.instance["name"]}"
+  }
+}
+
+resource "oci_core_instance" "redis" {
+  count               = "${var.instance["instance_count"]}"
+  availability_domain = "${element(data.template_file.ad_names.*.rendered, count.index)}"
+  fault_domain        = "FAULT-DOMAIN-${((count.index / length(data.template_file.ad_names.*.rendered)) % local.fault_domains_per_ad) +1}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "${var.instance["name"]}${count.index}"
+  shape               = "${var.instance["shape"]}"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.Subnet.id}"
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "${var.instance["name"]}${count.index}"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = "${var.images[var.region]}"
+  }
+
+  metadata {
+    ssh_authorized_keys = "${var.ssh_public_key}"
+    user_data           = "${base64encode(data.template_file.user_data.rendered)}"
+  }
+}
