3c12427ffbdac15b023bfdd68b295f4d86aa7c34
Collin Poczatek <collin.poczatek@gmail.com>
2020-12-11 16:54:51 -0800

    fix vars

--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
 variable "availablity_domain_name" {}
